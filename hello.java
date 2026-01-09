eef74bb7f6b0c3647338601217b0a0c7294f76e7
poojaraju-stud <poojaraju554@gmail.com>
2026-01-09 19:10:08 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.lang.System;
+class hello 
+{
+    public static void main(String[] args) 
+    {
+        int a=10;
+        int b=20;
+        int Sum=a+b;
+        int diff=a-b;
+        int div=a/b;
+        int mul=a*b;
+        int mod=a%b;
+        System.out.println("Sum:"+Sum);
+        System.out.println("diff:"+diff);
+        System.out.println("div:"+div);
+        System.out.println("mul:"+mul);
+        System.out.println("mod:"+mod);
+
+    }
+}
