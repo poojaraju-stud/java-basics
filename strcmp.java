ac6f82e6577e7fdf7c7fa31c088cb4d4e0e33292
poojaraju-stud <poojaraju554@gmail.com>
2026-01-13 19:34:57 +0530

    Add files via upload

---
+++ strcmp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class strcmp 
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        String RCB =sc.nextLine();
+
+        if(RCB.equals("win"))
+            System.out.println("Ee sala cup namdhey");
+
+        else
+            System.out.println("no cup");
+    }
+    
+}
