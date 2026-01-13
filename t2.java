ac295c2770a8ac109284499cfd603f602df274ce
poojaraju-stud <poojaraju554@gmail.com>
2026-01-13 19:54:42 +0530

    Add files via upload

---
+++ t2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class t2
+{
+    public static void main(String[] args)
+    {
+        Scanner ip=new Scanner(System.in);
+        int n=ip.nextInt();
+        if(n%2==0)
+            System.out.println("The number is even");
+        else
+            System.out.println("The number is odd");
+    }
+    
+}
